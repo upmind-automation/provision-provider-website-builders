0f9ec0858c875d4aac7803e292ef5c3c67383cd9
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2024-05-09 17:34:22 +0100

    use intval for floor value instead of int casting

--- src/Utils/Helpers.php
+++ src/Utils/Helpers.php
@@ -21,7 +21,7 @@ class Helpers
         }
 
         $size = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-        $factor = (int) floor((strlen((string)$bytes) - 1) / 3);
+        $factor = intval(floor((strlen((string)$bytes) - 1) / 3));
 
         if ($factor === 0 || $decimals < 0) {
             $decimals = 0;
